52aa801c2b74b7b7606125a75839bdb6767d9c23
lyllin3615 <3004208009@qq.com>
2020-11-03 00:47:57 +0800

    增加后端PHP项目

---
+++ php/Application/Runtime/Data/_fields/vuephp.user.php
@@ -0,0 +1,1 @@
+a:9:{i:0;s:2:"id";i:1;s:8:"username";i:2;s:8:"password";i:3;s:5:"phone";i:4;s:5:"email";i:5;s:9:"create_at";i:6;s:5:"token";s:3:"_pk";s:2:"id";s:5:"_type";a:7:{s:2:"id";s:16:"int(11) unsigned";s:8:"username";s:11:"varchar(20)";s:8:"password";s:11:"varchar(32)";s:5:"phone";s:7:"int(11)";s:5:"email";s:11:"varchar(40)";s:9:"create_at";s:7:"int(11)";s:5:"token";s:11:"varchar(32)";}}
